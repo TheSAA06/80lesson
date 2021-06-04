d0c3f4f81bd3c3098e9ad6cbdacad1b4cd81d3f0
TheSAA06 <60203764+TheSAA06@users.noreply.github.com>
2021-06-04 09:17:56 +0900

    Add files via upload

---
+++ attack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    int hp = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision Enter)
+    {
+        if(Enter.gameObject.tag == "enemy")
+        {
+            hp = hp - 1;
+            if(hp!=0)
+            {
+                print("Ударил врага");
+            }else{
+                print("Враг убит");
+            }
+        }
+    }
+}
---
+++ move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Rigidbody rb;
+    Animator anim;
+    float v;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        v = Input.GetAxis("Vertical");
+        rb.velocity = transform.forward * v * 5f;
+        if(v!=0)
+        {
+            anim.SetBool("isWalk", true);
+        }else
+        {
+            anim.SetBool("isWalk", false);
+        }
+        if(Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("attack");
+        }
+    }
+}
